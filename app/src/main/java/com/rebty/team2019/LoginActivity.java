aab907874f7ae2addad316ce6ba0803713e0de51
erement <erement@mail.ru>
2015-10-22 22:19:14 +0700

    FFA(First Fragment Add)

--- app/src/main/java/com/rebty/team2019/LoginActivity.java
+++ app/src/main/java/com/rebty/team2019/LoginActivity.java
@@ -1,13 +1,16 @@
 package com.rebty.team2019;
 
+import android.app.Fragment;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Fragment {
 
     EditText activity_login_editText_login;
     EditText activity_login_editText_password;
@@ -16,15 +19,14 @@ public class LoginActivity extends AppCompatActivity {
     Button activity_login_button_registration;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        activity_login_editText_login = (EditText) findViewById(R.id.activity_login_editText_login);
-        activity_login_editText_password = (EditText) findViewById(R.id.activity_login_editText_password);
-        activity_login_button_login = (Button) findViewById(R.id.activity_login_button_login);
-        activity_login_button_forgetPassword = (Button) findViewById(R.id.activity_login_button_forgetPassword);
-        activity_login_button_registration = (Button) findViewById(R.id.activity_login_button_registration);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_login, null);
+        activity_login_editText_login = (EditText) v.findViewById(R.id.activity_login_editText_login);
+        activity_login_editText_password = (EditText) v.findViewById(R.id.activity_login_editText_password);
+        activity_login_button_login = (Button) v.findViewById(R.id.activity_login_button_login);
+        activity_login_button_forgetPassword = (Button) v.findViewById(R.id.activity_login_button_forgetPassword);
+        activity_login_button_registration = (Button) v.findViewById(R.id.activity_login_button_registration);
 
         activity_login_button_registration.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,8 +34,8 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(v.getContext(), RegistrationActivity.class));
             }
         });
+        return v;
     }
-
 
 
 }
---
+++ app/src/main/java/com/rebty/team2019/MainActivity.java
@@ -0,0 +1,22 @@
+package com.rebty.team2019;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    Fragment loginact,regact;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
