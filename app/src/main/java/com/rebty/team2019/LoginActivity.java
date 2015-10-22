2c33a0dde6e9e04878aa5aa25d698d56d83466b4
TheSavagest <bormier@gmail.com>
2015-10-22 10:36:22 +0700

    Buttons&Activity
    добавлена анимация кнопок и Registration activity

--- app/src/main/java/com/rebty/team2019/LoginActivity.java
+++ app/src/main/java/com/rebty/team2019/LoginActivity.java
@@ -1,13 +1,39 @@
 package com.rebty.team2019;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText activity_login_editText_login;
+    EditText activity_login_editText_password;
+    Button activity_login_button_login;
+    Button activity_login_button_forgetPassword;
+    Button activity_login_button_registration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        activity_login_editText_login = (EditText) findViewById(R.id.activity_login_editText_login);
+        activity_login_editText_password = (EditText) findViewById(R.id.activity_login_editText_password);
+        activity_login_button_login = (Button) findViewById(R.id.activity_login_button_login);
+        activity_login_button_forgetPassword = (Button) findViewById(R.id.activity_login_button_forgetPassword);
+        activity_login_button_registration = (Button) findViewById(R.id.activity_login_button_registration);
+
+        activity_login_button_registration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), RegistrationActivity.class));
+            }
+        });
     }
+
+
+
 }
